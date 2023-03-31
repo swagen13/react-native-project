7b3d3e5e1f93a01e1d0eb118090f18fd21ce3cbe
swagen13 <106917556+swagen13@users.noreply.github.com>
2023-03-31 18:27:35 +0700

    first commit

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-import 'expo-router/entry';
+import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('main', () => App);
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
